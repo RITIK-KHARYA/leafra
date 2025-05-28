93c2678c4735d596dd6d644ef000df127219a945
ritik <ritikkharya05@gmail.com>
2025-05-28 21:34:40 +0530

    intial commut

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./db/schema.ts",
+  schema: "./app/db/schema.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  }
+
 });
--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,5 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
+  baseURL: "http://localhost:3000",
 });
